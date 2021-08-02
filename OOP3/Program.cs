ee0b8ba90d44998062d1b3d6dfa6053078c25e1b
AYBÜKE <AYBKE@DESKTOP-85KUE8U>
2021-08-02 11:44:44 +0300

    2 Agustos Ders Sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IKrediManager IhtiyacKrediManager  = new IhtiyacKrediManager();
+			IKrediManager TasitKrediManager  = new TasitKrediManager();
+			IKrediManager KonutKrediManager  = new KonutKrediManager();
+			IKrediManager EsnafKrediManager = new EsnafKrediManager();
+
+			ILoggerService databaseLoggerService = new DatabaseLoggerService();
+			ILoggerService fileLoggerService = new FileLoggerService();
+			ILoggerService smsLoggerService = new SmsLoggerService();
+
+			BasvuruManager basvuruManager = new BasvuruManager();
+			basvuruManager.BasvuruYap(EsnafKrediManager, new List<ILoggerService> { new DatabaseLoggerService(),new SmsLoggerService()}  );
+
+			List<IKrediManager> krediler = new List<IKrediManager>() { IhtiyacKrediManager,TasitKrediManager};
+			//basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+		}
+	}
+}
